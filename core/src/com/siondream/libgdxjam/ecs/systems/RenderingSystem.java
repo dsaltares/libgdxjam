afd400f2c4b55f6c258864877cc7a521f99ce9fa
David Saltares <david.saltares@gmail.com>
2015-12-19 18:45:42 +0000

    Unused imports

--- core/src/com/siondream/libgdxjam/ecs/systems/RenderingSystem.java
+++ core/src/com/siondream/libgdxjam/ecs/systems/RenderingSystem.java
@@ -13,8 +13,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
