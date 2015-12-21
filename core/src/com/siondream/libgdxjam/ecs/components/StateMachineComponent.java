5d22b1d975ac74268a1d04a673e1e6a9926541d2
unknown <alberto.cejas@ma2wks0150.ma2.gameloft.org>
2015-12-21 16:27:03 +0100

    +adds dummy statemachinecomponent

---
+++ core/src/com/siondream/libgdxjam/ecs/components/StateMachineComponent.java
@@ -0,0 +1,37 @@
+package com.siondream.libgdxjam.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+
+
+public class StateMachineComponent implements Component 
+{
+	/*private State m_previousState;
+	private State m_currentState;
+
+	private State m_globalState;
+	
+	public void setCurrentState(State s)
+	{
+		m_currentState = s;
+	}
+	
+	public void setPreviousState(State s)
+	{
+		m_previousState = s;
+	}
+	
+	public void setGlobalState(State s)
+	{
+		m_globalState = s;
+	}
+	
+	void update()
+	{
+		if(m_globalState)
+		{
+			m_globalState.execute();
+		}
+	}
+	
+	*/
+}
