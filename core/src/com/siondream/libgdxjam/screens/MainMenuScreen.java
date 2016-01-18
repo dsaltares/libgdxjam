d35e9b642b2e2b7ee4d4d03d6d9fb91b75d77b4a
AlbertoCejas <Alberto.Cejas.Sanchez@gmail.com>
2016-01-18 01:30:09 +0100

    +adds intro text

--- core/src/com/siondream/libgdxjam/screens/MainMenuScreen.java
+++ core/src/com/siondream/libgdxjam/screens/MainMenuScreen.java
@@ -9,13 +9,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Logger;
 import com.siondream.libgdxjam.Env;
 
